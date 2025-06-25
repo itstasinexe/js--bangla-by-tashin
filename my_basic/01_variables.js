6d554f5796f2d61a3ff2e43a14115835597bbeef
itstasinexe <tashankayef@gmail.com>
2025-06-25 07:48:33 +0000

    done

---
+++ my_basic/01_variables.js
@@ -0,0 +1,14 @@
+const account_ID = 158747;
+let account_email = "tashankayef@gmail.com";
+var account_pass = "3456";
+account_city = "sylhet";
+
+account_email = "tasfdd@gmaolng";
+account_pass = "fhgegf"
+account_city = "dhakja"
+// account_ID = 2;
+
+console.table([account_ID,account_email,account_pass,account_city])
+
+
+
